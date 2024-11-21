b9f5cbdbd35ffed7b2c4688be9acddaff347c731
JoaoIsaias <jl.isaias123@gmail.com>
2024-11-21 20:33:33 +0000

    Clicking on an emoji deletes it and refreshing the grid reverts it

--- BlissAplicationsChallenge/EmojiListViewModel.swift
+++ BlissAplicationsChallenge/EmojiListViewModel.swift
@@ -2,5 +2,15 @@ import Foundation
 import CoreData
 
 class EmojiListViewModel: ObservableObject {
+    func resetEmojisList(context: NSManagedObjectContext) -> [Emoji]? {
+        let fetchRequest: NSFetchRequest<Emoji> = Emoji.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Emoji.name, ascending: true)]
+        do {
+            return try context.fetch(fetchRequest)
+        } catch {
+            print("Failed to fetch items: \(error.localizedDescription)")
+        }
 
+        return nil
+    }
 }
